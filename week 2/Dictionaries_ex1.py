a0bccbbe3f97c86958f0a528648024857b0bbb41
Nurdaulet Zhumatayev <nodzhymun705@gmail.com>
2024-02-08 15:22:03 +0600

    check

--- week 2/Dictionaries_ex1.py
+++
@@ -1,8 +0,0 @@
-car =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-print(car.get["model"])
-#or
-#print(car["model"])
--- week 2/ifelse_ex6.py
+++
@@ -1,6 +0,0 @@
-a=3
-b=5
-c=3
-d=3
-if a == b or c == d:
-  print("Hello")
