12ebe2bbc8a7bd2ea20204ee4f8d572c52f61831
Nurdaulet Zhumatayev <nodzhymun705@gmail.com>
2024-02-21 00:36:14 +0600

    lab4

---
+++ week3/week4/generators.py
@@ -0,0 +1,35 @@
+#task 1
+"""
+number=input()
+for i in range(int(number)):
+    print(pow(i+1, 2))
+"""
+#task 2
+"""
+number=input()
+for i in range(int(number)+1):
+    if i%2==0 and i!=0:
+        print(i)
+"""
+#task 3
+"""
+number=input() 
+for i in range(int(number)+1):
+    if i%3==0 and i%4==0 and i!=0:
+        print(i)
+"""
+#task 4
+"""
+print("from?")
+iz=input()
+print("to?")
+to=input()
+for i in range(int(iz),int(to)+1):
+    print(pow(i, 2))
+"""
+#task 5
+"""
+number=input()
+for i in range(int(number), 0-1, -1):
+    print(i)
+"""
