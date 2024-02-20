237e070a234c6443367917bbda58c40a6d60a3d5
Nurdaulet Zhumatayev <nodzhymun705@gmail.com>
2024-02-21 01:23:53 +0600

    lab4

---
+++ week3/week4/math.py
@@ -0,0 +1,31 @@
+#task 1
+"""
+gradus=input("Input degree: ")
+radian=float(int(gradus)*3.14159265359/180)
+print("Output radian: ", radian)
+"""
+#task 2
+"""
+height=input("Height: ")
+first=input("Base, first value: ")
+second=input("Base, second value: ")
+value=((int(first)+int(second))/2*int(height))
+print("Expected Output: ", value)
+"""
+#task 3
+"""
+import math
+number=input("Input number of sides: ")
+length=input("Input the length of a side: ")
+angle = math.pi / int(number)
+cotangent = 1 / math.tan(angle)
+value=(1/4)*(int(number))*(int(length)**2)*cotangent
+print("The area of the polygon is: ", value)
+"""
+#task 4
+"""
+length=input("Length of base: ")
+height=input("Height of parallelogram: ")
+value=int(length)*int(height)
+print("Expected Output: ", float(value))
+"""
