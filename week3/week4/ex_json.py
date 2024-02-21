c653670c692310944cf84519f363b320e71786e6
Nurdaulet Zhumatayev <nodzhymun705@gmail.com>
2024-02-21 19:32:29 +0600

    lab4

--- week3/week4/ex_json.py
+++ week3/week4/ex_json.py
@@ -1,19 +1,11 @@
 import json
-
-with open('sample-data.json') as file:
-    json_data = json.load(file)
-
-
-
-
-
 print("Interface Status")
 print("================================================================================")
 print("DN                                                 Description           Speed    MTU")  
 print("-------------------------------------------------- --------------------  ------  ------")
-
+with open("sample-data.json", "r") as file:
+    json_data = json.load(file)
 iamdata=json_data["imdata"]
-
 for item in iamdata:
     nt_item = item["l1PhysIf"]
     att = nt_item["attributes"]
