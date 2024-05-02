ed9cffca8481ad27e5070a2d03482b471d8405a7
Nurdaulet Zhumatayev <nodzhymun705@gmail.com>
2024-05-02 14:37:29 +0600

    lab 11

--- week 11/ex1/ex5.py
+++ week 11/ex1/ex5.py
@@ -7,23 +7,32 @@ cur = conn.cursor()
 def inputData():
     name = input("Hello input your name: ")
     number = input("Input your phone number: ")
-    cur.execute(' INSERT INTO phone_book("PersonName", "PhoneNumber") VALUES( %s, %s); ' , (name, number))
+    cur.execute(' INSERT INTO deleter("PersonName", "PhoneNumber") VALUES( %s, %s); ' , (name, number))
 
 def deleteData():
     print("Which name do ypu want to delete?\n")
     personName = input()
-    cur.execute(f''' DELETE FROM phone_book WHERE "PersonName"='{personName}' ''')
+    cur.execute(f''' DELETE FROM deleter WHERE "PersonName"='{personName}' ''')
+
+def deleteNum():
+    print("Which num do ypu want to delete?\n")
+    personNum = input()
+    cur.execute(f''' DELETE FROM deleter WHERE "PhoneNumber"='{personNum}' ''')
 done = False
 while not done:
     print("What do you want to do?\n\
           1. Input data from console\n\
-          2. Delete data from table by person name")
-    x = int(input("Enter number 1-5\n"))
+          2. Delete data from table by person name\n\
+          3. Delete data from table by person num")
+    x = int(input("Enter number 1-3\n"))
     if(x == 1):
         inputData()
    
     elif(x == 2):
         deleteData()
+
+    elif(x == 3):
+        deleteNum()
     conn.commit()
     
 cur.close()
